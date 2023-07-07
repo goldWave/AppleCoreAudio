db1be3e70d8547c65f1852145044d27ecbf6a23b
jimbo <ren.jinbo@navercorp.com>
2023-07-07 20:12:13 +0800

    Initial Commit

---
+++ CoreAudioDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CoreAudioDemo
+//
+//  Created by jimbo on 2023/7/7.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ CoreAudioDemo/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  CoreAudioDemo
+//
+//  Created by jimbo on 2023/7/7.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
