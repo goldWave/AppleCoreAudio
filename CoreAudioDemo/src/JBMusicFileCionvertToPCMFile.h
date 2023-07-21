10df5f74eea02bb05715b34a84d7ef25b9564e89
jimbo <ren.jinbo@navercorp.com>
2023-07-21 14:34:12 +0800

    重置文件夹结构

---
+++ CoreAudioDemo/src/JBMusicFileCionvertToPCMFile.h
@@ -0,0 +1,16 @@
+//
+//  JBMusicFileCionvertToPCMFile.h
+//  CoreAudioDemo
+//
+//  Created by jimbo on 2023/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JBMusicFileCionvertToPCMFile : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CoreAudioDemo/src/JBMusicFileCionvertToPCMFile.m
@@ -0,0 +1,12 @@
+//
+//  JBMusicFileCionvertToPCMFile.m
+//  CoreAudioDemo
+//
+//  Created by jimbo on 2023/7/21.
+//
+
+#import "JBMusicFileCionvertToPCMFile.h"
+
+@implementation JBMusicFileCionvertToPCMFile
+
+@end
